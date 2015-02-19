dc23f82db209ccd7bd8244fe10d0fb26daff2ad3
Anton Belev <belevanton@gmail.com>
2015-02-19 22:08:08 +0000

    weights improved for CTO

--- CTO.java
+++ CTO.java
@@ -1,12 +1,13 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
 
 /**
  * @since 16/02/2015
@@ -14,14 +15,29 @@ import java.util.Set;
  * Cipher Text Only Attack
  */
 public class CTO {
+	private static final String BIGRAMS_TXT = "bigrams.txt";
+	private static final String C2_TXT = "c2.txt";
+	private static final String WORDS_TXT = "words.txt";
 	public static Set<String> englishWords = new HashSet<String>();
+	public static Map<String, Double> bigramFrequencyTable = new HashMap<String, Double>();
 	public static void main(String[] args) throws FileNotFoundException{
 		setUpEnglishWordsSet();
+		setUpBigramFrequencyTable();
 		ctoCheckForEnglishWords();
 	}
 	
+	private static void setUpBigramFrequencyTable() throws FileNotFoundException {
+		Scanner s = new Scanner(new File(BIGRAMS_TXT));
+		while (s.hasNextLine()){
+			String[] bigramFreq = s.nextLine().split(" ");
+			double score = Double.parseDouble(bigramFreq[1]);
+			bigramFrequencyTable.put(bigramFreq[0].toLowerCase(), score);
+		}
+		s.close();
+	}
+
 	private static void setUpEnglishWordsSet() throws FileNotFoundException {
-		Scanner s = new Scanner(new File("words"));
+		Scanner s = new Scanner(new File(WORDS_TXT));
 		while (s.hasNextLine()){
 			englishWords.add(s.nextLine().toLowerCase());
 		}
@@ -30,76 +46,51 @@ public class CTO {
 
 	private static void ctoCheckForEnglishWords() throws FileNotFoundException{
 		ArrayList<KeyScorePair> keyScores = new ArrayList<KeyScorePair>();
-		String fileIn = "c2.txt";
+		String fileIn = C2_TXT;
 		ArrayList<String> cipherBlocks = DecryptAllBlocks.getCipherBlocks(fileIn);
 		for (int key = 0; key < Math.pow(2, 16); key++){
 			String keyHexString = intToHexString(key);
 			ArrayList<String> decryptedBlocks = DecryptAllBlocks.decrypt(cipherBlocks, keyHexString);
 			String text = Block2Text.block2Text(decryptedBlocks);
-			int score = scoreTextBasedOnPresenceOfEnglishWords(text);
-			keyScores.add(new KeyScorePair(keyHexString, score));
+			int validEnglishScore = scoreValidEnglishWords(text);
+			double bigramScore = scoreBigramFrequency(text);
+			keyScores.add(new KeyScorePair(keyHexString, validEnglishScore + bigramScore));
 		}
-		KeyScorePair maxScore = new KeyScorePair("-1", -1);
+		KeyScorePair maxScore = new KeyScorePair("-1", Double.MIN_VALUE);
 		for (KeyScorePair kc : keyScores){
 			if (kc.score > maxScore.score){
+				//System.out.println("new score " + kc.score + " key " + kc.hexKeyString);
 				maxScore = kc;
+				System.out.println("Potential key decimal - " + maxScore.hexKeyString + " Score for key: " + maxScore.score);
+				ArrayList<String> dblocks = DecryptAllBlocks.decrypt(cipherBlocks, maxScore.hexKeyString);
+				String text = Block2Text.block2Text(dblocks);
+				System.out.println("Decrypted text:****************\n" + text + "****************\n");
 			}
 		}
-		System.out.println("Potential key decimal - " + maxScore.hexKeyString + " Score for key: " + maxScore.score);
-		ArrayList<String> dblocks = DecryptAllBlocks.decrypt(cipherBlocks, maxScore.hexKeyString);
-		String text = Block2Text.block2Text(dblocks);
-		System.out.println("Decrypted text:\n" + text);
+		
 	}
 	
-	private static void ctoCheckEnglishLetters() throws IOException{
-		for (int key = 0; key < Math.pow(2, 16); key++){
-			String fileIn = "c2.txt";
-			String fileOut = "c2_decrypted.txt";	
-			String keyHexString = intToHexString(key);
-			if(key%1000 == 0)
-				System.out.println(keyHexString);
-			String[] arguments = {fileIn, fileOut, keyHexString};
-			DecryptAllBlocks.main(arguments);
-			String[] arguments2 = {"c2_decrypted"};
-			//System.out.println(keyHexString);
-			Block2Text.main(arguments2);
-			if (checkDecryptedTextForEnglishChars("c2_decrypted_t.txt")){
-				System.out.println("Potential key decimal - " + key 
-						+ " hex - " + keyHexString);
-				DecryptCipher.decrypt("c2", keyHexString);
-			}
-		}
-	}
-	
-	private static boolean checkDecryptedTextForEnglishChars(String fileName) throws IOException {
-		File f = new File(fileName);
-	    InputStream targetStream = new FileInputStream(f);
-	    int nextByte = targetStream.read();
-		while (nextByte != -1 ){
-			if (!isLatinLetter(nextByte)){
-				targetStream.close();
-				return false;
-			}
-			nextByte = targetStream.read();
-		}
-		targetStream.close();
-		return true;
-	}
-	
-	private static int scoreTextBasedOnPresenceOfEnglishWords(String text){
+	private static int scoreValidEnglishWords(String text){
 		String[] words = text.split(" ");
 		int score = 0;
 		for (String word : words){
-			if (englishWords.contains(word.toLowerCase())){
-				score += 100;
-			}
+			if (englishWords.contains(word.toLowerCase()))
+				score += 100*word.length();
+			else
+				score -= 10*word.length();
 		}
 		return score;
 	}
 	
-	public static boolean isLatinLetter(int c) {
-	    return (c >= (int)'A' && c <= (int)'Z') || (c >= (int)'a' && c <= (int)'z')
-	    		|| c == (int)' ' || c == (int)'.' || c == (int)',' || c == (int)'\'' || c == (int)'!' || c == (int)'?';
+	private static double scoreBigramFrequency(String text){
+		text = text.replaceAll("[^a-zA-Z ]", " ").toLowerCase();
+		double score = 0.0;
+		for (int i = 0; i < text.length()-1; i++){
+			String bigram = text.substring(i, i+2);
+			if (bigramFrequencyTable.containsKey(bigram))
+				score += bigramFrequencyTable.get(bigram);
+		}
+		return score;
 	}
 
 	private static String intToHexString(int i){
@@ -108,8 +99,8 @@ public class CTO {
 	
 	private static class KeyScorePair{
 		public String hexKeyString;
-		public int score;
-		public KeyScorePair(String hexKeyString, int score) {
+		public double score;
+		public KeyScorePair(String hexKeyString, double score) {
 			this.hexKeyString = hexKeyString;
 			this.score = score;
 		}
--- KPT.java
+++ KPT.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 
 /**
  * @since 16/02/2015
@@ -9,15 +12,20 @@ public class KPT {
 	private static String plainTextFristBlock = "0x446f";
 	private static String cipherTextFirstBlock = "0xd1b2";
 	private static String cipherTextFirstBlockStringDecrypted = "";
+	private static final String C1_TXT = "c1.txt";
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		ArrayList<String> cipherBlocks = DecryptAllBlocks.getCipherBlocks(C1_TXT);
 		for (int key = 0; key < Math.pow(2, 16); key++){
 			cipherTextFirstBlockStringDecrypted = decrypt(intToHexString(key), cipherTextFirstBlock);
 			if (cipherTextFirstBlockStringDecrypted.equals(plainTextFristBlock)){
-				System.out.println("Potential key decimal - " + key 
-						+ " hex - " + intToHexString(key));
-				DecryptCipher.decrypt("c1", intToHexString(key));
+				System.out.println("Key found. Decimal value - " + key 
+						+ " Hex value - " + intToHexString(key));
+				ArrayList<String> dblocks = DecryptAllBlocks.decrypt(cipherBlocks, intToHexString(key));
+				String text = Block2Text.block2Text(dblocks);
+				System.out.println("Decrypted text:\n" + text);
+				break;
 			}
 		}
 	}
@@ -28,13 +36,10 @@ public class KPT {
 	
 	private static String decrypt(String hexString, String cipherBlock){
 		int	key = Hex16.convert(hexString);
-		for (;;)
-		{
-			int	c = Hex16.convert(cipherBlock);
-			int	p = Coder.decrypt(key, c);
-			String	out = String.format("0x%04x", p);
-			return out;
-		}
+		int	c = Hex16.convert(cipherBlock);
+		int	p = Coder.decrypt(key, c);
+		String	out = String.format("0x%04x", p);
+		return out;
 	}
 
 }
